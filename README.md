4b3c2fcf2986815d4ed9d3d7d702f3fb6cfdd2d0
firas1awadallah <firasawa@gmail.com>
2023-03-11 17:53:50 +0000

    add event

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # HR-management-system
 
-lab 07:
+## lab 07
 
-1. Create two pages index.html and accounting.html.
+1 Create two pages index.html and accounting.html.
 
-2. Create app.js page .
+2 Create app.js page .
 
-3. Create a constructor to generate an employee object.
+3 Create a constructor to generate an employee object.
 
-4. Create a prototype function for calculating the salary.
+4 Create a prototype function for calculating the salary.
+
+## lab 08
+
+1 Creat a form.
+
+2 Create a function to generate a unique id.
+
+3 add an event listener to get the data from the form.
+
+4 Creat render prototype function to render each employee information.
--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 "use strict";
-function Employee (Employee_ID , Full_Name , Department ,Level ,Image_URL){
-    this.employeeID = Employee_ID;
+ function Employee (Full_Name , Department ,Level ,Image_URL){
+    this.employeeID = 0;
     this.fullName = Full_Name;
     this.department = Department;
     this.level = Level;
     this.imageUrl = Image_URL;
     this.salary = 0;
-    }
+ }
 
-let employee1 = new Employee(1000,"Ghazi Samer","Administration","Senior");
-let employee2 = new Employee(1001,"Lana Ali","Finance","Senior");
-let employee3 = new Employee(1002,"Tamara Ayoub","Marketing","Senior");
-let employee4 = new Employee(1003,"Safi Walid","Administration","Mid-Senior");
-let employee5 = new Employee(1004,"Omar Zaid","Development","Senior");
-let employee6 = new Employee(1005,"Rana Saleh","Development","Junior");
-let employee7 = new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior");
+      Employee.prototype.creatId = function  () {
+        this.employeeID =  Math.floor(1000 + Math.random() * 9000);
+      
+} 
 
-Employee.prototype.netSalary = function  () {
+      Employee.prototype.netSalary = function  () {
     
         if(this.level =="Junior"){
             this.salary = Math.floor(((Math.random() * (1000 - 500) ) + 500)* 0.925) ;
@@ -24,27 +21,65 @@ Employee.prototype.netSalary = function  () {
         } 
         else if(this.level =="Mid-Senior"){
             this.salary = Math.floor(((Math.random() * (1500 - 1000) ) + 1000)* 0.925) ;
-      }
+        }
        else if(this.level =="Senior"){
         this.salary = Math.floor(((Math.random() * (2000 - 1500) ) + 1500)* 0.925) ;
+       }
      }
-    }
-    Employee.prototype.render  = function  () {
-        document.write(`<h4>Name: ${this.fullName} <br> Salary: ${this.salary}</h4><hr>`);
-    }
-    employee1.netSalary();
-    employee2.netSalary();
-    employee3.netSalary();
-    employee4.netSalary();
-    employee5.netSalary();
-    employee6.netSalary();
-    employee7.netSalary();
+     
+      Employee.prototype.render  = function  () {
+          document.write(`<div style="color:white ;text-align: center;background-color:#3CB371;width:25%"> <img style="margin:20px"src="${this.imageUrl}" <br><h4>  Name: ${this.fullName} -ID: ${this.employeeID}<br> Department: ${this.department} -Level: ${this.level}<br> Salary: ${this.salary}</h4><br></div>`);
+          }
     
-    employee1.render();
-    employee2.render();
-    employee3.render();
-    employee4.render();
-    employee5.render();
-    employee6.render();
-    employee7.render();
+     let form = document.getElementById("form");
+     form.addEventListener("submit",gitSubmit);
+     
+      function gitSubmit (event) {
+        event.preventDefault();
+       
+       
+        let Full_Name = event.target.Fullname.value;
+        let Department = event.target.Department.value;
+        let Level = event.target.Level.value;
+        let Image_URL = event.target.Image.value;
+        
+        let newEmployee = new Employee (Full_Name, Department, Level, Image_URL);
+         newEmployee.creatId();
+         newEmployee.netSalary();
+         newEmployee.render();
+     }
+
+     
+    
+    
+    
+
+
+   // let employee1 = new Employee(1000,"Ghazi Samer","Administration","Senior");
+   // let employee2 = new Employee(1001,"Lana Ali","Finance","Senior");
+   // let employee3 = new Employee(1002,"Tamara Ayoub","Marketing","Senior");
+   // let employee4 = new Employee(1003,"Safi Walid","Administration","Mid-Senior");
+   // let employee5 = new Employee(1004,"Omar Zaid","Development","Senior");
+   // let employee6 = new Employee(1005,"Rana Saleh","Development","Junior");
+   // let employee7 = new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior");
+
+
+    // employee1.creatId();
+    
+    // employee1.netSalary();
+    // employee2.netSalary();
+    // employee3.netSalary();
+    // employee4.netSalary();
+    // employee5.netSalary();
+    // employee6.netSalary();
+    // employee7.netSalary();
+    
+    // employee1.render();
+    // employee2.render();
+    // employee3.render();
+    // employee4.render();
+    // employee5.render();
+    // employee6.render();
+    // employee7.render();
+
     
